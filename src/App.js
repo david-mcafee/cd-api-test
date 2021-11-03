7428c295010e0935d952ddfdf1a6088868b5dc62
David McAfee <mcafd@amazon.com>
2021-11-02 17:11:50 -0700

    render queried items

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { API, graphqlOperation } from "aws-amplify";
+// I recognized the unfortunate auto-naming of this query...
+import { listClasss } from "./graphql/queries";
+import {
+  Header,
+  Icon,
+  List,
+  ListItem,
+  ListContent,
+  ListHeader,
+} from "semantic-ui-react";
+import { useStyles } from "./styles";
 
-function App() {
+const initialClassState = [];
+
+const App = () => {
+  const [classes, setClasses] = useState(initialClassState);
+
+  const { container, parentContainer } = useStyles();
+
+  useEffect(() => {
+    fetchClasses();
+  }, []);
+
+  async function fetchClasses() {
+    try {
+      const classData = await API.graphql(graphqlOperation(listClasss));
+      const classes = classData?.data?.listClasss?.items;
+      console.log(classData);
+      setClasses(classes);
+    } catch (error) {
+      console.log("error fetching classes", error);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={parentContainer}>
+      <div className={container}>
+        <Header as="h1" icon textAlign="center">
+          <Icon name="users" circular />
+          <Header.Content>My Classes</Header.Content>
+          <Header sub>CD API test</Header>
+        </Header>
+        <List>
+          {classes.map((classItem) => (
+            <ListItem key={classItem.id}>
+              <ListContent>
+                <ListHeader>
+                  <p>{classItem.name}</p>
+                </ListHeader>
+              </ListContent>
+            </ListItem>
+          ))}
+        </List>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
